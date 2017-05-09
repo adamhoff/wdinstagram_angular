eb8e382e23a965f9f366e1427253e969d06bf9a1
Adam Hoff <swimmerdude79@comcast.net>
2017-05-09 09:56:20 -0400

    part one

--- js/app.js
+++ js/app.js
@@ -1,5 +1,44 @@
 "use strict";
 
-(function(){
-  angular.module("wdinstagram", [])
-})();
+
+
+
+angular
+  .module("Entry", [
+    "ui.router"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+  .controller("EntryIndexController", [
+    EntryIndexControllerFunction
+  ])
+  .controller("EntryShowController", [
+    "$stateParams",
+    EntryShowControllerFunction
+  ]);
+
+function RouterFunction($stateProvider) {
+  $stateProvider
+    .state("entryIndex", {
+      url: "/entries",
+      templateUrl: "js/ng-views/index.html",
+      controller: "EntryIndexController",
+      controllerAs: "vm"
+    })
+    .state("entryShow", {
+      url: "/entries/:id",
+      templateUrl: "js/ng-views/show.html",
+      controller: "EntryShowController",
+      controllerAs: "vm"
+    })
+}
+
+function EntryIndexControllerFunction() {
+  this.entries = entries;
+}
+
+function EntryShowControllerFunction($stateParams) {
+  this.entry = entries[$stateParams.id];
+}
